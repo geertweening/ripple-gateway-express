b2c4f4e4d808bbd84785a2a198a14c21c986c08f
Geert Weening <geertweening@gmail.com>
2014-03-10 18:25:52 -0700

    [CHORE] start on fixing up gateway-express tests

--- test/controllers/external_accounts.js
+++ test/controllers/external_accounts.js
@@ -3,6 +3,8 @@ var sinon = require('sinon');
 var adapter = require('../../lib/adapter.js');
 var controllers = require('../../lib/controllers/index.js')(adapter);
 
+adapter = adapter.externalAccounts;
+
 describe("external accounts controller", function(){
   before(function(){
     req = {
@@ -13,36 +15,37 @@ describe("external accounts controller", function(){
       send: function(data){  }
     };
   });
+
   it("should create an external account", function(fn) {
-    adapter.createExternalAccount = sinon.spy();
+    adapter.create = sinon.spy();
     req.body.userId = 1;
     req.body.name = 'someaccount';
     controllers.external_accounts.create(req, res);
-    assert(adapter.createExternalAccount.called);
+    assert(adapter.create.called);
     fn();
   });
   it("should get an external account", function(fn) {
-    adapter.getExternalAccount = sinon.spy();
+    adapter.read = sinon.spy();
     controllers.external_accounts.show(req, res);
-    assert(adapter.getExternalAccount.called);
+    assert(adapter.read.called);
     fn();
   });
   it("should get many external accounts", function(fn) {
-    adapter.getExternalAccounts = sinon.spy();
+    adapter.readAll = sinon.spy();
     controllers.external_accounts.index(req, res);
-    assert(adapter.getExternalAccounts.called);
+    assert(adapter.readAll.called);
     fn();
   });
   it("should update an external account", function(fn) {
-    adapter.updateExternalAccount = sinon.spy();
+    adapter.update = sinon.spy();
     controllers.external_accounts.update(req, res);
-    assert(adapter.updateExternalAccount.called);
+    assert(adapter.update.called);
     fn();
   });
   it("should delete an external account", function(fn) {
-    adapter.deleteExternalAccount = sinon.spy();
+    adapter.delete = sinon.spy();
     controllers.external_accounts.delete(req, res);
-    assert(adapter.deleteExternalAccount.called);
+    assert(adapter.delete.called);
     fn();
   });
 });
--- test/controllers/users.js
+++ test/controllers/users.js
@@ -1,39 +1,46 @@
-var assert = require('assert');
-var sinon = require('sinon');
-var adapter = require('../../lib/adapter.js');
+var assert      = require('assert');
+var sinon       = require('sinon');
+var adapter     = require('../../lib/adapter.js');
 var controllers = require('../../lib/controllers/index.js')(adapter);
+var crypto      = require('crypto');
+
+function rand() {
+  return crypto.randomBytes(32).toString('hex');
+}
 
 describe("users controller", function(){
   before(function(){
     req = {
-      body: {}  
+      name: rand(),
+      password: rand(),
+      body: {}
     };
     res = {
       send: function(){}
     };
   });
   it("should create a user", function(fn) {
-    adapter.createUser = sinon.spy();
+    adapter.create = sinon.spy();
     controllers.users.create(req, res);
-    assert(adapter.createUser.called);
+    assert(adapter.create.called);
     fn();
   });
   it("should get a user", function(fn) {
-    adapter.getUser = sinon.spy();
+    adapter.read = sinon.spy();
     controllers.users.show(req, res);
-    assert(adapter.getUser.called);
+    assert(adapter.read.called);
     fn();
   });
   it("should update a user", function(fn) {
-    adapter.updateUser = sinon.spy();
+    adapter.update = sinon.spy();
     controllers.users.update(req, res);
-    assert(adapter.updateUser.called);
+    assert(adapter.update.called);
     fn();
   });
   it("should delete a user", function(fn) {
-    adapter.deleteUser = sinon.spy();
+    adapter.delete = sinon.spy();
     controllers.users.delete(req, res);
-    assert(adapter.deleteUser.called);
+    assert(adapter.delete.called);
     fn();
   });
 });
